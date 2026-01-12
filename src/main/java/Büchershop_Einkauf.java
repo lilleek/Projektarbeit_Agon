d6dd3ce2837462b13ffb275ff7de7b948078bf01
lilleek <noahleonringle@gmail.com>
2026-01-12 19:23:18 +0100

    Ich habe eine Klasse angelegt in der man dann später die Bücher eingeben kann. Hier sollen auch die Preise errechnet werden.

---
+++ src/main/java/Büchershop_Einkauf.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Büchershop_Einkauf {
+    private JButton speichernButton;
+    private JButton ausgebenButton;
+    private JTextField textField1;
+    private JPanel kaufPanel;
+
+    public Büchershop_Einkauf() {
+
+
+    }
+}
