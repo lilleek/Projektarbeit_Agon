741bc9e079bcdab610ed08e31530905ad2d0a745
lilleek <noahleonringle@gmail.com>
2026-01-12 19:28:22 +0100

    Ich habe den Button hinzugefügt

--- src/main/java/Büchershop.java
+++ src/main/java/Büchershop.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Büchershop extends JFrame {
     private JPanel mainPanel;
     private JPasswordField passwordField1;
     private JPanel Willkommen;
     private JPanel Wilkommen;
+    private JButton klickButton;
 
     public Büchershop() {
         setTitle("Willkommen im Büchershop");
@@ -12,7 +15,14 @@ public class Büchershop extends JFrame {
         setContentPane(mainPanel);
         setVisible(true);
 
+        klickButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Büchershop_Einkauf();
+            }
+        });
     }
+
 
     static void main() {
         new Büchershop();
