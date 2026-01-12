cd09208799d1e8642b2a57ced3f2a97dff04c794
lilleek <noahleonringle@gmail.com>
2026-01-12 18:28:07 +0100

    Erneutes Hochladen der gemeinsamen Arbeit bis jetzt

---
+++ src/main/java/Büchershop.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Büchershop extends JFrame {
+    private JPanel mainPanel;
+    private JPasswordField passwordField1;
+    private JPanel Willkommen;
+    private JPanel Wilkommen;
+
+    public Büchershop() {
+        setTitle("Willkommen im Büchershop");
+        setSize(800, 200);
+        setContentPane(mainPanel);
+        setVisible(true);
+
+    }
+
+    static void main() {
+        new Büchershop();
+    }
+}
