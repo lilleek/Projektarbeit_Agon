58a949992b65a4bd20008377a7e75091d47be0bf
lilleek <noahleonringle@gmail.com>
2026-01-12 21:18:08 +0100

    Ich habe den neuen Button gebaut und verknüpft. Anschließend habe ich den dispose befehl eingefügt damit sich das erste Fenster schließt.

--- src/main/java/Büchershop.java
+++ src/main/java/Büchershop.java
@@ -7,19 +7,30 @@ public class Büchershop extends JFrame {
     private JPasswordField passwordField1;
     private JPanel Willkommen;
     private JPanel Wilkommen;
-    private JButton klickButton;
+    private JButton KaufenButton;
+    private JButton verkaufenButton;
 
     public Büchershop() {
         setTitle("Willkommen im Büchershop");
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 200);
         setContentPane(mainPanel);
         setVisible(true);
 
-        klickButton.addActionListener(new ActionListener() {
+        KaufenButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                dispose();
                 new Büchershop_Einkauf();
+            }
+        });
+
+
+        verkaufenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                new Büchershop_Verkauf();
             }
         });
     }
