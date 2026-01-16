9398e2ba2ad8d081c372b8f85f9b7194b592f087
Agonios <kurzfil@gmail.com>
2026-01-16 14:06:36 +0100

    +added Test

---
+++ src/test/java/Büchershop_EinkaufTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Büchershop_EinkaufTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void clearButton() {
+    }
+
+    @Test
+    void testMain() {
+    }
+
+    @Test
+    void testClearButton() {
+    }
+
+    @Test
+    void testMain1() {
+    }
+
+    @Test
+    void testClearButton1() {
+    }
+
+    @Test
+    void testMain2() {
+    }
+
+    @Test
+    void testClearButton2() {
+    }
+}
